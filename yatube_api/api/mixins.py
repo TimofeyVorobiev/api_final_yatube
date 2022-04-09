d7bcc1627627c578b719dc0835245a175d7008da
Timofey Vorobiev <vorobiev.te@yandex.ru>
2022-04-09 11:21:34 +0300

    2

--- yatube_api/api/mixins.py
+++ yatube_api/api/mixins.py
@@ -4,4 +4,3 @@ from rest_framework import mixins, viewsets
 class FollowCreateListSet(mixins.CreateModelMixin, mixins.ListModelMixin,
                           viewsets.GenericViewSet):
     pass
-
